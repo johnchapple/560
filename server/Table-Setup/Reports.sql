3ca882c11435d699c8cd5634a95e50cad50baa05
Jordan Greene <greenjor000@gmail.com>
2020-11-19 00:25:45 -0600

    Added Reports.sql

---
+++ server/Table-Setup/Reports.sql
@@ -0,0 +1,32 @@
+/**Report For Total Monthly Sales For Each Year**/
+SELECT MONTHNAME(STR_TO_DATE(MONTH(OrderDate), '%m')) AS Month, SUM(PurchasedPrice) AS TotalMonthlySales
+FROM restaurant.orders
+JOIN purchasedmenuitem ON restaurant.orders.OrderID = purchasedmenuitem.OrderID
+WHERE YEAR(OrderDate) = '2019'
+GROUP BY MONTH(OrderDate)
+ORDER BY MONTH(OrderDate) ASC
+
+/**Report For Total Sales Per Employee For Month/Year**/
+SELECT CONCAT(FirstName, " ", LastName) AS EmployeeName, SUM(PurchasedPrice) AS TotalSales
+FROM restaurant.orders AS O
+JOIN restaurant.purchasedmenuitem ON O.OrderID = restaurant.purchasedmenuitem.OrderID
+JOIN restaurant.employees ON O.EmployeeID = restaurant.employees.EmployeeID
+WHERE YEAR(OrderDate) = '2019' OR '2020'
+GROUP BY O.EmployeeID
+ORDER BY TotalSales DESC
+
+/**Report For Best Selling Menu Items Year/Month**/
+SELECT restaurant.menuitem.name AS ItemName, COUNT(restaurant.purchasedmenuitem.MenuItemID) AS NumOrdered
+FROM restaurant.purchasedmenuitem
+JOIN restaurant.menuitem ON restaurant.purchasedmenuitem.MenuItemID = restaurant.menuitem.MenuItemID
+JOIN restaurant.orders ON restaurant.purchasedmenuitem.OrderID = restaurant.orders.OrderID
+WHERE MONTH(restaurant.orders.OrderDate) = MONTH(NOW()) AND YEAR(restaurant.orders.OrderDate) = YEAR(NOW())
+GROUP BY restaurant.purchasedmenuitem.MenuItemID 
+
+/**Forecast For Next Month's Inventory Ordering Using THis Month's Sold Items**/
+SELECT restaurant.menuitem.name AS ItemName, COUNT(restaurant.purchasedmenuitem.MenuItemID) AS NumOrdered, ROUND(COUNT(restaurant.purchasedmenuitem.MenuItemID) * 1.5) AS Forecast
+FROM restaurant.purchasedmenuitem
+JOIN restaurant.menuitem ON restaurant.purchasedmenuitem.MenuItemID = restaurant.menuitem.MenuItemID
+JOIN restaurant.orders ON restaurant.purchasedmenuitem.OrderID = restaurant.orders.OrderID
+WHERE MONTH(restaurant.orders.OrderDate) = MONTH(NOW()) AND YEAR(restaurant.orders.OrderDate) = YEAR(NOW())
+GROUP BY restaurant.purchasedmenuitem.MenuItemID 
