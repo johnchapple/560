e75d6c8523d147a20b8dbca8bda8f70e40aa90be
Alex Varenhorst <alexvarenhorst@gmail.com>
2020-11-19 11:15:35 -0600

    Commits for chart

--- client/src/ReportPage.vue
+++ client/src/ReportPage.vue
@@ -4,27 +4,47 @@
 
 <template>
 	<!-- <pre> -->
-		<!-- {{reportChart}} -->
+	<div id="alex" onload="createChart()" style="height: 400px; background-color: black;"></div>
 </template>
 
 
 <script>
-import Highcharts from 'highcharts';
-import { createHighcharts } from 'vue-highcharts';
- 
-import loadMap from 'highcharts/modules/map.js';
- 
-loadMap(Highcharts);
+	import Highcharts from 'highcharts';
 
 
-export default {
-  components: {
-    Highcharts: createHighcharts('Highcharts', Highcharts),
-    // Highmaps: createHighcharts('Highmaps', Highcharts),
-    // Highstock: createHighcharts('Highstock', Highcharts),
-    // HighchartsGantt: createHighcharts('HighchartsGantt', Highcharts),
-  },
-};
+
+	export default {
+  		name: 'Reports',
+  		mounted(){
+  			this.createChart();
+  		},
+  		methods: {
+  			createChart(){
+  				console.log("hey");
+		     Highcharts.chart('alex', {		
+			    title: {
+			        text: 'Point interval unit is one month'
+			    },
+
+			    xAxis: {
+			        type: 'datetime'
+			    },
+
+			    plotOptions: {
+			        series: {
+			            pointStart: Date.UTC(2015, 0, 1),
+			            pointIntervalUnit: 'month'
+			        }
+			    },
+			    series: [{
+			        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+			    }, {
+			        data: [144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4, 129.2]
+			    }]
+			});
+  			}
+  		}
+	}
 </script>
 
 	<!-- graphs showing sales as a company (line/bar)-->
