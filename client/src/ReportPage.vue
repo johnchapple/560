b616a932d5cdd72fc5a8f754a95ac4fabdf8f24b
Alex Varenhorst <alexvarenhorst@gmail.com>
2020-11-19 10:51:47 -0600

    ReportPage.vue, and reports.js

---
+++ client/src/ReportPage.vue
@@ -0,0 +1,32 @@
+
+
+<!-- <script src="https://code.highcharts.com/highcharts.js"></script> -->
+
+<template>
+	<!-- <pre> -->
+		<!-- {{reportChart}} -->
+</template>
+
+
+<script>
+import Highcharts from 'highcharts';
+import { createHighcharts } from 'vue-highcharts';
+ 
+import loadMap from 'highcharts/modules/map.js';
+ 
+loadMap(Highcharts);
+
+
+export default {
+  components: {
+    Highcharts: createHighcharts('Highcharts', Highcharts),
+    Highmaps: createHighcharts('Highmaps', Highcharts),
+    // Highstock: createHighcharts('Highstock', Highcharts),
+    // HighchartsGantt: createHighcharts('HighchartsGantt', Highcharts),
+  },
+};
+</script>
+
+	<!-- graphs showing sales as a company (line/bar)-->
+	<!-- graphs showing sales per person (line) -->
+	<!-- other ideas -->
---
+++ server/routes/reports.js
@@ -0,0 +1,19 @@
+exports.all = function(req, res){
+
+    if(req.method == "GET"){
+      var sql = "SELECT * FROM Employees"
+  
+
+
+      var query = db.query(sql, function(err, result) {
+        res.send(result)
+      });
+      /*
+      res.send([
+        { id: 0, FirstName: 'John', LastName: 'Chapple', JoinedDate: (new Date('January 1, 2018')) },
+        { id: 1, FirstName: 'Mitch', LastName: 'McConnell', JoinedDate: (new Date('February 2, 2019')) },
+        { id: 2, FirstName: 'Mike', LastName: 'Pence', JoinedDate: (new Date('March 3, 2020')) }
+      ])
+      */
+    }
+   };
